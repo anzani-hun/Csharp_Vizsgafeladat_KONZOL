9692b33a5eebdd035abad32f9b72639d71763293
anzani-hun <anzani@hun>
2024-05-02 17:52:44 +0200

    csv javítása

---
+++ Csharp_Vizsgafeladat_KONZOL/Epulet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Vizsgafeladat_KONZOL
+{
+    class Epulet
+    {
+        // adatbázis első sora: 1;Burj Khalifa;Dubai (AE);828;2717;163;2010;steel/concrete;office / residential / hotel
+        //int string string decimal int int int
+        public int rang;
+        public string nev;
+        public string varos_orszag;
+        public decimal magassag_meter;
+        public int magassag_lab;
+        public int emeletekSzama;
+        public int epitesEve;
+
+
+        //konstruktor generálása
+        public Epulet(int rang, string nev, string varos_orszag, decimal magassag_meter, int magassag_lab, int emeletekSzama, int epitesEve)
+        {
+            this.rang = rang;
+            this.nev = nev;
+            this.varos_orszag = varos_orszag;
+            this.magassag_meter = magassag_meter;
+            this.magassag_lab = magassag_lab;
+            this.emeletekSzama = emeletekSzama;
+            this.epitesEve = epitesEve;
+        }
+    }
+}
--- Csharp_Vizsgafeladat_KONZOL/Program.cs
+++ Csharp_Vizsgafeladat_KONZOL/Program.cs
@@ -4,13 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//csv eléréséhez kell
+using System.IO;
+
 namespace Csharp_Vizsgafeladat_KONZOL
 {
     class Program
     {
-        static void Main(string[] args)
-        {
 
+        //dinamikus lista létrehozása (epuletek) néven: 
+        static List<Epulet> epuletek = new List<Epulet>();
+
+    static void Main(string[] args)
+        {
+            beolvasas();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Az adatbázis beolvasása!");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (Epulet adat in epuletek)
+            {
+                Console.WriteLine(adat.nev);
+            }
+
+
+            
+            
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        private static void beolvasas()
+        {
+            using (StreamReader sr = new StreamReader("100_tallest_javitott4.csv"))
+            {
+                //olvassa amíg nincs vége a fájlnak
+                while (!sr.EndOfStream)
+                {
+                    //a sorokból vesszőnél elválasztva feldaraboljuk
+                    string[] line = sr.ReadLine().Split(';');
+
+                    //Epulet példányosítása     
+                    // adatbázis első sora: 1;Burj Khalifa;Dubai (AE);828;2717;163;2010;steel/concrete;office / residential / hotel
+                    Epulet epulet = new Epulet(int.Parse(line[0]), line[1], line[2], decimal.Parse(line[3]), int.Parse(line[4]), int.Parse(line[5]), int.Parse(line[6]));
+
+                    //epuletek hozzáadása soronként
+                    epuletek.Add(epulet);
+                }
+            }
         }
     }
 }
